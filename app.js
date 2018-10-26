2ad62cb840ac7afcc6ad1a339c50f9b536d5e42c
Sean king <sean7thomasking@gmail.com>
2018-10-26 02:02:59 +0100

    added poller

--- app.js
+++ app.js
@@ -4,25 +4,44 @@ var app = express();
 var router = express.Router();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
 const RabbitService = {};
 RabbitService.setupRabbit = require('./services/rabbitService/setup-rabbit').setupRabbit;
 
+
 // Consume Messages and 'broadcast' over all open client connections
 let messages = io.of('/messages');
-let channel;
+
+// let channel;
+const env = require('dotenv').config(); // for local testing
 const sMessagingserviceUri = process.env.RABBIT_MQ_LOCAL;
 
 
 app.listen(3030, () => console.log(`client-message-emitter-service listening on port 3030!`));
 
-while (!channel) {
-   startConsumer(channel);
+let channel;
+
+const poller = async () => {
+   try {
+      if (!channel) {
+         startConsumer(channel);
+      }
+   } catch (err) {
+      log.logMessage("error", "Connection to rabbitMQ unsuccessful");
+   } finally {
+      if (!channel)
+         setTimeout(() => {
+            poller();
+         }, 2500)
+   }
 }
 
-async function startConsumer(channel){
+setImmediate(poller);
+
+async function startConsumer(channel) {
    channel = await RabbitService.setupRabbit(sMessagingserviceUri);
    if (channel)
-      channel.consume("InvoiceQueue", async function (msg) {
+      channel.consume("NewMessageQueue", async function (msg) {
          let message = JSON.parse(msg.content.toString());
          message.uuid = uuid;
          messageId = message.messageId;
